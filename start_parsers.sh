d1abcae63df05eb93bb03a54bc59db116172d722
la-strole <la-strole.sovaleto48@gmail.com>
2021-11-05 18:48:25 +0300

    modify start_parsers to use requirements.txt

--- start_parsers.sh
+++ start_parsers.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 path_line='/home/ub/Documents/GIT/parsers'
-source "$path_line/venv/bin/activate" &>> "$path_line/log.txt"
+python3 -m venv "$path_line/parsers_venv" &>> "$path_line/log.txt"
+source "$path_line/parsers_venv/bin/activate" &>> "$path_line/log.txt"
+pip install -r "$path_line/requirements.txt" &>> "$path_line/log.txt"
 python "$path_line/main.py" &>> "$path_line/log.txt"
 deactivate
