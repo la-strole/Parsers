6d35ac3fe037020c98a104d3cf8d4725594e464a
la-strole <la-strole.sovaleto48@gmail.com>
2021-11-06 13:34:53 +0300

    task: change default working directory to /home/ub/Documents/GIT/parsers

--- create_environment.sh
+++ create_environment.sh
@@ -1,4 +1,5 @@
-path_line='/home/ub/Documents/GIT/parsers'
+#!/bin/bash
+path_line=$PWD
 python3 -m venv "$path_line/parsers_venv" &>> "$path_line/log.txt"
 source "$path_line/parsers_venv/bin/activate" &>> "$path_line/log.txt"
 pip install -r "$path_line/requirements.txt" &>> "$path_line/log.txt"
--- start_parsers.sh
+++ start_parsers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-path_line='/home/ub/Documents/GIT/parsers'
+path_line=$PWD
 source "$path_line/parsers_venv/bin/activate" &>> "$path_line/log.txt"
 python "$path_line/main.py" -path "$path_line/" &>> "$path_line/log.txt"
 deactivate
